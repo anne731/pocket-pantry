8dccb1f8c2788e2fe2bbcda5d882d95b44bfde96
Anne Silbaugh <Anne.Silbaugh@blackbaud.me>
2019-07-29 15:57:45 -0400

    app-shell stuff

--- pocket-pantry/src/main.ts
+++ pocket-pantry/src/main.ts
@@ -1,6 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, Injectable, ApplicationRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { concat, interval } from 'rxjs';
+import { first } from 'rxjs/operators';
 
+import { SwUpdate } from '@angular/service-worker';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +11,47 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+document.addEventListener('DOMContentLoaded', () => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+});
+
+@Injectable()
+export class LogUpdateService {
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+    });
+    updates.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
+  }
+}
+
+@Injectable()
+export class CheckForUpdateService {
+
+  constructor(appRef: ApplicationRef, updates: SwUpdate) {
+    // Allow the app to stabilize first, before starting polling for updates with `interval()`.
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+  }
+}
+
+// @Injectable()
+// export class PromptUpdateService {
+
+//   constructor(updates: SwUpdate) {
+//     updates.available.subscribe(event => {
+//       if (promptUser(event)) {
+//         updates.activateUpdate().then(() => document.location.reload());
+//       }
+//     });
+//   }
+// }
