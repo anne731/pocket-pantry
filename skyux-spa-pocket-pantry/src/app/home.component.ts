b90878d844627b5d5a71d615cc023a7957319315
Anne Silbaugh <Anne.Silbaugh@blackbaud.me>
2019-07-29 18:20:12 -0400

    food object and pantry component

--- skyux-spa-pocket-pantry/src/app/home.component.ts
+++ skyux-spa-pocket-pantry/src/app/home.component.ts
@@ -2,8 +2,51 @@ import {
   Component
 } from '@angular/core';
 
+//import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+
+export class FoodItem {
+  name: string;
+  expiration: string = null; // form: MM-DD-YY:HH:MM:SS
+  datePurchased: string = null;
+  servingsLeft: number = 1;
+  foodType: string = "";
+
+  constructor (name: string,
+    expiration: string,
+    datePurchased: string,
+    servingsLeft: number,
+    foodType: string
+    )
+    {
+      this.name = name;
+      this.expiration = expiration;
+      this.datePurchased = datePurchased;
+      this.servingsLeft = servingsLeft;
+      this.foodType = foodType;
+
+      if (this.datePurchased == "null") {
+        var today = new Date();
+        var date = today.getFullYear()+"-"+(today.getMonth()+1)+'-'+today.getDate()
+              + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        this.datePurchased = date;
+      }
+      if (this.foodType == "") {
+        //webscrape to determine type!
+      }
+      if (this.expiration == null) {
+        //webscrape to find shelf life
+        // add shelf life to todays date to get expiration
+      }
+    }
+
+}
+
+export class HomeComponent {
+
+
+}
