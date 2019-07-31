8f5ffd040ca26ea733acf818d7365445fcd6dc8f
Anne Silbaugh <Anne.Silbaugh@blackbaud.me>
2019-07-30 22:50:27 -0400

    making fridge and pantry same

--- skyux-spa-pocket-pantry/src/app/pantry-component/pantry.component.ts
+++ skyux-spa-pocket-pantry/src/app/pantry-component/pantry.component.ts
@@ -2,97 +2,12 @@ import {
   Component
 } from '@angular/core';
 
-import {
-  Observable
-} from 'rxjs/Observable';
-
-import 'rxjs/add/observable/of';
-
-import {
-  SkyModalService,
-  SkyModalCloseArgs
-} from '@skyux/modals';
-import { AddItemComponent } from '../add-item/add-item.component';
+//import { FridgeComponent } from '../fridge-component/fridge.component';
 
 @Component({
-  selector: 'app-pantry',
-  templateUrl: './pantry.component.html'
+  templateUrl: '../fridge-component/fridge.component.html'
 })
 export class PantryComponent {
-  
-  constructor(
-    private modal: SkyModalService
-  ) { }
-  private idNum: number = 1;
-  public valueA: string;
-  public eventMessage?: string;
-  public iconGroupSelectedValue = 'table';
-  public items = Observable.of([
-    { id: this.idNum , column1: 'Popcorn', column2: '8.30.2020', column3: 'TBD', column4: "Snack" }
-    // { id: '2', column1: '', column2: '', column3: '', column4: ""},
-    // { id: '3', column1: '', column2: '', column3: '', column4: "" },
-    // { id: '4', column1: '', column2: '', column3: '', column4: "" },
-    // { id: '5', column1: '', column2: '', column3: '', column4: "" },
-    // { id: '6', column1: '', column2: '', column3: '', column4: "" },
-    // { id: '7', column1: '', column2: '', column3: '', column4: "" }
-  ]);
 
-  get ID(): number {
-    return this.idNum;
-  }
-  set ID(value:number) {
-    this.idNum = value;
-  }
-
-  public onFilterButtonClicked(): void {
-    alert('Filter button clicked');
-  }
-
-  public onSummaryItemClick(): void {
-    alert('Filter summary item clicked');
-  }
-
-  public onAddButtonClicked(): void {
-    const options: any = {
-      ariaDescribedBy: 'docs-modal-content'
-    };
-
-    this.eventMessage = "help me";
-
-    const modalInstance = this.modal.open(AddItemComponent, options.helpKey);
-    // modalInstance.closed.subscribe((result: SkyModalCloseArgs) => {
-    //   console.log(`Modal closed with reason: ${result.reason} and data: ${result.data}`);
-    // });
-
-    modalInstance.helpOpened.subscribe((helpKey: string) => {
-      this.eventMessage =  `
-        Modal header help was invoked with the following help key: ${helpKey}
-      `;
-    });
-
-    modalInstance.closed.subscribe((result: SkyModalCloseArgs) => {
-      // if (result.reason === 'save') {
-      //   if (result.data.) {
-      //     requiredFieldsArray = result.data.requiredFields.split(',').map(function(e: string) {
-      //       return e.trim();
-      //     });
-      //   }
-      if (result.reason === 'save') {
-        let items = {
-          column1: result.data[0],
-          column2: result.data[1],
-          column3: result.data[2],
-          column4: result.data[3]
-
-        }
-        console.log(result.data);
-      }
-        console.log(result.reason);
-        console.log(result);
-        // this.items.subscribe({id: this.ID, column1:  }) ;
-        // this.ID++;
-      // }
-    });
-  }
 
 }
